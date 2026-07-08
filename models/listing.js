63f8a0e56a7ddd740cf5f552392de067e82dbe47
SMYousufDev <syedmuhammadyousuf23@gmail.com>
2026-07-08 21:11:48 +0500

    added a listing.js file in models

---
+++ models/listing.js
@@ -0,0 +1,24 @@
+
+const mongoose = require ("mongoose");
+const Schema = mongoose.Schema;
+
+const listingSchema = new Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  description: String,
+  image: {
+    type: String,
+    set: (v) =>
+      v === ""
+        ? "https://wallpaperaccess.com/full/31201.jpg"
+        : v,
+  },
+  price: Number,
+  location: String,
+  country: String,
+});
+
+const Listing = mongoose.model("Listing", listingSchema);
+modules.export = Listing;
