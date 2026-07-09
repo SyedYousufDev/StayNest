cd348e3f92c53fc8cc426fc8e4975768b063b891
SMYousufDev <syedmuhammadyousuf23@gmail.com>
2026-07-09 23:55:01 +0500

    updation in app.js

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+const Listing = require("../StayNest/models/listing.js");  
 
 const Mongo_URL = "mongodb://127.0.0.1:27017/StayNest";
 
@@ -24,4 +25,19 @@ app.get("/", (req, res) => {
 
 app.listen(8080, () => {
     console.log("Server is started on port 8080");
+});
+
+//testing
+app.get("/testListing", async (req, res) => {
+  let sampleListing = new Listing({
+    title: "My New Distination",
+    description: "By the Mountains",
+    price: 1200,
+    location: "Mingora, Swat",
+    country: "Pakistan",
+  });
+
+  await sampleListing.save();
+  console.log("sample was saved");
+  res.send("successful testing");
 });
