a1f55ee96522b1efd88bccdcdcdf8ef984e30f04
SMYousufDev <syedmuhammadyousuf23@gmail.com>
2026-07-15 12:51:31 +0500

    feat: show route

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const path = require("path");
 
 app.set("view engine","ejs");
 app.set("views",path.join(__dirname,"views"));
+app.use(express.urlencoded({extended: true}));
 
 const Mongo_URL = "mongodb://127.0.0.1:27017/StayNest";
 
@@ -57,3 +58,10 @@ app.get("/listings", async (req, res) => {
   const allListings = await Listing.find({});
   res.render("listings/index.ejs", {allListings});
 });
+
+//show route
+app.get("/listings/:id",async (req,res)=>{
+    let {id}= req.params;
+    const listing = await Listing.findById(id);
+    res.render("listings/show.ejs",{listing});
+}) 
