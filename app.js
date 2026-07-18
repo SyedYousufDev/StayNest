8063fe06b118d9881a5d25e9075eb71bbbcc8c50
SMYousufDev <syedmuhammadyousuf23@gmail.com>
2026-07-18 21:55:40 +0500

    routes: new and create

--- app.js
+++ app.js
@@ -59,6 +59,19 @@ app.get("/listings", async (req, res) => {
   res.render("listings/index.ejs", {allListings});
 });
 
+//New route
+app.get("/listings/new",async (req,res)=>{
+    
+    res.render("listings/new.ejs" );
+}) 
+
+//Create Route
+app.post("/listings", async (req, res) => {
+    const newListing = new Listing(req.body.listing);
+    await newListing.save();
+    res.redirect("/listings");
+});
+
 //show route
 app.get("/listings/:id",async (req,res)=>{
     let {id}= req.params;
