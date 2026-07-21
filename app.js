c5e62af2662b996eca35ff82679749c3f8ed5192
SMYousufDev <syedmuhammadyousuf23@gmail.com>
2026-07-21 19:09:07 +0500

    edit and update route addition

--- app.js
+++ app.js
@@ -4,9 +4,12 @@ const mongoose = require("mongoose");
 const Listing = require("../StayNest/models/listing.js");
 const path = require("path");
 
+const methodOverride = require("method-override");
+
 app.set("view engine","ejs");
 app.set("views",path.join(__dirname,"views"));
 app.use(express.urlencoded({extended: true}));
+app.use(methodOverride("_method"));
 
 const Mongo_URL = "mongodb://127.0.0.1:27017/StayNest";
 
@@ -78,3 +81,18 @@ app.get("/listings/:id",async (req,res)=>{
     const listing = await Listing.findById(id);
     res.render("listings/show.ejs",{listing});
 }) 
+
+//Edit Route
+
+app.get("/listings/:id/edit", async (req,res)=>{
+    let { id } = req.params;
+    const listing = await Listing.findById(id);
+    res.render("listings/edit.ejs",{ listing })
+})
+
+//update route
+app.put("/listings/:id",async (req,res)=>{
+    let { id } = req.params;
+    await Listing.findByIdAndUpdate(id, {...req.body.listing});
+    res.redirect("/listings");
+})
