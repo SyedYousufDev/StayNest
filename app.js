c08c5a20419a78791bb4ec922835273ca34dbf0d
SMYousufDev <syedmuhammadyousuf23@gmail.com>
2026-09-13 14:48:17 +0500

    added a link

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ app.set("views",path.join(__dirname,"views"));
 app.use(express.urlencoded({extended: true}));
 app.use(methodOverride("_method"));
 app.engine('ejs',ejsMate);
+app.use(express.static(path.join(__dirname, "public")));
 
 const Mongo_URL = "mongodb://127.0.0.1:27017/StayNest";
 
